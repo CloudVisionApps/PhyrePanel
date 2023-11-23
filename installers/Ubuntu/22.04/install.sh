c7fb061860b656859597c018f1e1ac856941db77
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-11-24 00:08:30 +0200

    Update install.sh

--- installers/Ubuntu/22.04/install.sh
+++ installers/Ubuntu/22.04/install.sh
@@ -41,6 +41,7 @@ DEPENDENCIES_LIST=(
     "php8.2-mbstring"
     "php8.2-curl"
     "php8.2-xml"
+    "php8.2-intl"
     "php8.2-pear"
     "php8.2-bcmath"
     "lsb-release"
@@ -107,7 +108,8 @@ php8.2 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php8.2 -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php8.2 composer-setup.php
 php8.2 -r "unlink('composer-setup.php');"
-php8.2 composer.phar install --no-dev --optimize-autoloader --no-interaction
+
+COMPOSER_ALLOW_SUPERUSER=1 php8.2 composer.phar install --no-dev --optimize-autoloader --no-interaction
 
 # Configure the application
 cp .env.example .env
