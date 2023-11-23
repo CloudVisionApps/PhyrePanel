3661540de5f7d64d9c80018a051196288ba004a4
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-11-24 00:34:53 +0200

    Update create-mysql-db-and-user.sh

--- shell/helpers/ubuntu/create-mysql-db-and-user.sh
+++ shell/helpers/ubuntu/create-mysql-db-and-user.sh
@@ -10,15 +10,15 @@ function create_mysql_db_and_user() {
   fi
 
   mysql -u root <<MYSQL_SCRIPT
-  CREATE DATABASE $2;
-  CREATE USER '$1'@'localhost' IDENTIFIED BY '$PASS';
-  GRANT ALL PRIVILEGES ON $2.* TO '$1'@'localhost';
+  CREATE DATABASE $1;
+  CREATE USER '$2'@'localhost' IDENTIFIED BY '$PASS';
+  GRANT ALL PRIVILEGES ON $1.* TO '$2'@'localhost';
   FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
   echo "MySQL user and database created."
-  echo "Username: $1"
   echo "Database: $1"
+  echo "Username: $2"
   echo "Password: $PASS"
   echo "Success!"
 
