9d1ec319efdfe9891782fefac9d42a7a59304541
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-11-25 13:30:21 +0200

    Update nginx-compile.sh

--- compilators/debian/nginx/nginx-compile.sh
+++ compilators/debian/nginx/nginx-compile.sh
@@ -16,28 +16,27 @@ sudo make
 sudo make install
 
 sudo mkdir $MAIN_DIR/alphax-nginx-1.20.0
-cd $MAIN_DIR/alphax-nginx-1.20.0
-sudo mkdir $MAIN_DIR/DEBIAN
-sudo mkdir $MAIN_DIR/usr
-sudo mkdir $MAIN_DIR/usr/local
-sudo mkdir $MAIN_DIR/usr/local/alphax
-sudo mkdir $MAIN_DIR/etc/init.d
+PACKAGE_MAIN_DIR=$MAIN_DIR/alphax-nginx-1.20.0
 
-sudo mv /usr/local/alphax/nginx/sbin/nginx $MAIN_DIR/usr/local/alphax/nginx/sbin/alpha-nginx
-sudo mv /usr/local/alphax/nginx $MAIN_DIR/usr/local/alphax
-sudo cp $MAIN_DIR/control DEBIAN
+sudo mkdir $PACKAGE_MAIN_DIR/DEBIAN
+sudo mkdir $PACKAGE_MAIN_DIR/usr
+sudo mkdir $PACKAGE_MAIN_DIR/usr/local
+sudo mkdir $PACKAGE_MAIN_DIR/usr/local/alphax
+sudo mkdir $PACKAGE_MAIN_DIR/etc/init.d
 
-sudo cp $MAIN_DIR/etc/init.d/alphax $MAIN_DIR/alphax
-sudo chmod +x $MAIN_DIR/etc/init.d/alphax
-ls
+sudo mv /usr/local/alphax/nginx $PACKAGE_MAIN_DIR/usr/local/alphax
 
-cd ../
+sudo mv $PACKAGE_MAIN_DIR/usr/local/alphax/nginx/sbin/nginx $PACKAGE_MAIN_DIR/usr/local/alphax/nginx/sbin/alpha-nginx
+sudo cp $PACKAGE_MAIN_DIR/control DEBIAN
 
-sudo dpkg-deb --build alphax-nginx-1.20.0
-sudo dpkg --info alphax-nginx-1.20.0.deb
-sudo dpkg --contents alphax-nginx-1.20.0.deb
+sudo cp $MAIN_DIR/alphax $PACKAGE_MAIN_DIR/etc/init.d/alphax
+sudo chmod +x $PACKAGE_MAIN_DIR/etc/init.d/alphax
 
-sudo mv alphax-nginx-1.20.0.deb $MAIN_DIR/dist
+sudo dpkg-deb --build $PACKAGE_MAIN_DIR/alphax-nginx-1.20.0
+sudo dpkg --info $PACKAGE_MAIN_DIR/alphax-nginx-1.20.0.deb
+sudo dpkg --contents $PACKAGE_MAIN_DIR/alphax-nginx-1.20.0.deb
+
+sudo mv $PACKAGE_MAIN_DIR/alphax-nginx-1.20.0.deb $MAIN_DIR/dist
 
 cd $MAIN_DIR
 ls
