c99e1d17e26c3b6f07ae63a79f29ffaef9d24bd3
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-11-27 01:12:27 +0200

    update

--- web/app/Models/Domain.php
+++ web/app/Models/Domain.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
 
 class Domain extends Model
 {
-    use HasFactory;
+    use Sushi;
+
+    public function getRows()
+    {
+        return [
+            ['id' => 1, 'label' => 'admin'],
+            ['id' => 2, 'label' => 'manager'],
+            ['id' => 3, 'label' => 'user'],
+        ];
+    }
+
 }
